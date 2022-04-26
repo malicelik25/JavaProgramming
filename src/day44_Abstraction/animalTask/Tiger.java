44274ff328cf46e54c47cfc8a76511195bcd7ec5
Muhammed Ali Çelik <muhammedalicelikk@gmail.com>
2022-04-26 15:13:19 +0300

    tiger

--- src/day44_Abstraction/animalTask/Tiger.java
+++ src/day44_Abstraction/animalTask/Tiger.java
@@ -1,6 +1,6 @@
 package day44_Abstraction.animalTask;
 
-public class Tiger extends Animal{
+public final class Tiger extends Animal{
 
     public Tiger(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
@@ -9,5 +9,6 @@ public class Tiger extends Animal{
     @Override
     public void eat() {
         System.out.println(getName() + " eats deer");
+
     }
 }
