9fbb7c85f5bbe9e18083d6d77a0b003834c25e7b
Muhammed Ali Çelik <muhammedalicelikk@gmail.com>
2022-04-26 15:11:31 +0300

    cat

--- src/day44_Abstraction/animalTask/Cat.java
+++ src/day44_Abstraction/animalTask/Cat.java
@@ -1,6 +1,6 @@
 package day44_Abstraction.animalTask;
 
-public class Cat extends Animal{
+public final class Cat extends Animal{
 
 
     public Cat(String name, String breed, char gender, int age, String size, String color) {
