0b46c5f0028092afca20a06e1e8c0488c0056a0a
Muhammed Ali Çelik <muhammedalicelikk@gmail.com>
2022-05-03 15:06:25 +0300

    interface

--- src/day44_Abstraction/animalTask/Tiger.java
+++ src/day44_Abstraction/animalTask/Tiger.java
@@ -1,6 +1,6 @@
 package day44_Abstraction.animalTask;
 
-public final class Tiger extends Animal{
+public final class Tiger extends Animal implements WildAnimal{
 
     public Tiger(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
@@ -10,5 +10,10 @@ public final class Tiger extends Animal{
     public void eat() {
         System.out.println(getName() + " eats deer");
 
+    }
+
+    @Override
+    public void hunt() { // public is the only access modifier we can give when we override interface
+        System.out.println(getName() + " hunts deer");
     }
 }
