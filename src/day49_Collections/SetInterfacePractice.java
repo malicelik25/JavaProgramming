3b53b89ddbb9f9fcb3f84eef6c7392edccf9987b
Muhammed Ali Çelik <muhammedalicelikk@gmail.com>
2022-05-16 16:20:39 +0300

    Set Interface Practice

---
+++ src/day49_Collections/SetInterfacePractice.java
@@ -0,0 +1,40 @@
+package day49_Collections;
+
+import java.util.*;
+
+public class SetInterfacePractice {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        list.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        list.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        list.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        list.addAll(Arrays.asList(10, 200, 300, 40, 90));
+
+        System.out.println("list = " + list);
+        System.out.println(list.get(4));
+
+
+        Set<Integer> hashSet = new HashSet<>();
+        hashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        hashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        hashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        hashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        hashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        hashSet.addAll(Arrays.asList(null, null, null));
+
+        System.out.println("hashSet = " + hashSet);
+        //System.out.println(hashSet.getClass(4));You can not use the index number
+
+
+        Set<Integer> linkedHashSet = new LinkedHashSet<>();
+
+
+        Set<Integer> treeSet = new TreeSet<>();
+
+
+    }
+
+}
