70c871d5a5d1a41b8fe5e44dfb4038c9203f32fa
Muhammed Ali Çelik <muhammedalicelikk@gmail.com>
2022-05-25 17:15:59 +0300

    FirstFunctionalInterface

---
+++ src/day52_Map_FunctionalInterface/MyFirstFunctionalInterface.java
@@ -0,0 +1,10 @@
+package day52_Map_FunctionalInterface;
+
+@FunctionalInterface
+public interface MyFirstFunctionalInterface {
+
+    void apply( int n);
+
+    //void method(); // only contains 1 abstract method(gives compile error)
+
+}
